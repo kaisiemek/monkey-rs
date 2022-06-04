cc78dad85996a4e174d9b80315963e78dc0e7ff7
Kai Siemek <kai.siemek@protonmail.com>
2022-06-04 21:37:54 +0200

    tests: added parser return stmt tests

--- src/ast.rs
+++ src/ast.rs
@@ -2,15 +2,20 @@ use crate::token::Token;
 
 pub type Program = Vec<Statement>;
 
+#[derive(Debug)]
 pub enum Statement {
     LetStmt {
         token: Token,
         identifier: String,
         value: Expression,
     },
-    ReturnStmt,
+    ReturnStmt {
+        token: Token,
+        value: Expression,
+    },
 }
 
+#[derive(Debug)]
 pub enum Expression {
     IdentifierExpr,
 }
--- src/parser_test.rs
+++ src/parser_test.rs
@@ -1,5 +1,6 @@
 #[cfg(test)]
 mod tests {
+    use core::panic;
     use std::collections::VecDeque;
 
     use crate::{
@@ -12,45 +13,22 @@ mod tests {
     #[test]
     fn test_let_statements() {
         let input = concat!("let x = 5;\n", "let y = 10;\n", "let foobar = 838383;\n",);
+        let mut expected_identifiers: VecDeque<&str> = VecDeque::from_iter(["x", "y", "foobar"]);
 
         let lexer = Lexer::new(input);
         let mut parser = Parser::new(lexer);
 
-        let program: Program;
-        match parser.parse_program() {
-            Ok(prog) => program = prog,
-            Err(err_vec) => {
-                let errs = err_vec.join("\n");
-                assert!(
-                    false,
-                    "The parser encountered {} errors:\n{}",
-                    err_vec.len(),
-                    errs
-                );
-                return;
-            }
-        }
-
-        assert_eq!(
-            program.len(),
-            3,
-            "Expected parser to parse 3 statements, parsed {} instead",
-            program.len()
-        );
-
-        let mut expected_identifiers: VecDeque<&str> = VecDeque::from_iter(["x", "y", "foobar"]);
+        let program = parse_program(&mut parser, expected_identifiers.len());
         let mut statements = VecDeque::from_iter(program);
 
         while !expected_identifiers.is_empty() {
             let expected_identifier = expected_identifiers.pop_front().unwrap();
             let current_stmt = statements.pop_front().unwrap();
-
-            let result = test_let_statement(current_stmt, expected_identifier);
-            assert!(matches!(result, Ok(())));
+            test_let_statement(current_stmt, expected_identifier);
         }
     }
 
-    fn test_let_statement(stmt: Statement, expected_name: &str) -> Result<(), String> {
+    fn test_let_statement(stmt: Statement, expected_name: &str) {
         let stmt_token: Token;
         let stmt_identifier: String;
         let stmt_value: Expression;
@@ -66,26 +44,68 @@ mod tests {
                 stmt_value = value;
             }
             _ => {
-                return Err(String::from(
-                    "Statement was not an instance of a let statement",
-                ));
+                assert!(false, "Expected a LetStatement, got: {:?}", &stmt);
+                panic!();
             }
-        };
-
-        if stmt_token.literal != "let" {
-            return Err(format!(
-                "The token literal was not 'let', got {} instead",
-                stmt_token.literal
-            ));
         }
 
-        if stmt_identifier != expected_name {
-            return Err(format!(
-                "Expected identifier value {} but got {}",
-                expected_name, stmt_identifier
-            ));
-        }
+        assert_eq!(
+            stmt_token.literal, "let",
+            "The token literal was not 'let', got {} instead",
+            stmt_token.literal
+        );
 
-        Ok(())
+        assert_eq!(
+            stmt_identifier, expected_name,
+            "Expected identifier value {} but got {}",
+            expected_name, stmt_identifier
+        );
+    }
+
+    #[test]
+    fn test_return_statements() {
+        let input = concat!("return 5;\n", "return 10;\n", "return 838383;\n",);
+        let lexer = Lexer::new(input);
+        let mut parser = Parser::new(lexer);
+
+        let program: Program = parse_program(&mut parser, 3);
+        let mut statements = VecDeque::from_iter(program);
+
+        while !statements.is_empty() {
+            let current_stmt = statements.pop_front().unwrap();
+            if let Statement::ReturnStmt { token, .. } = current_stmt {
+                assert_eq!(
+                    token.literal, "return",
+                    "Expected statement literal to be 'return' but got '{}'",
+                    token.literal
+                );
+            } else {
+                assert!(false, "Expected ReturnStatement, got {:?}", current_stmt);
+            }
+        }
+    }
+
+    fn parse_program(parser: &mut Parser, expected_statements: usize) -> Program {
+        match parser.parse_program() {
+            Ok(prog) => {
+                assert_eq!(
+                    prog.len(),
+                    expected_statements,
+                    "Expected parser to parse 3 statements, parsed {} instead",
+                    prog.len()
+                );
+                return prog;
+            }
+            Err(err_vec) => {
+                let errs = err_vec.join("\n");
+                assert!(
+                    false,
+                    "The parser encountered {} errors:\n{}",
+                    err_vec.len(),
+                    errs
+                );
+                panic!();
+            }
+        }
     }
 }
