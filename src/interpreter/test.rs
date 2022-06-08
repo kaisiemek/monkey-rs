eafdd703786b4ae513161bdb6dac66e18b036bea
Kai Siemek <kai.siemek@protonmail.com>
2022-06-08 21:40:13 +0200

    test: added test modules to silence warnings again

--- src/interpreter/test.rs
+++ src/interpreter/test.rs
@@ -1,64 +1,65 @@
-use crate::{
-    lexer::Lexer,
-    parser::{ast::Node, Parser},
-};
-
-use super::{eval, object::Object};
-
 #[cfg(test)]
-#[test]
-fn test_integer_expression() {
-    struct TestCase<'a> {
-        input: &'a str,
-        expected: isize,
-    }
+mod test {
+    use crate::{
+        interpreter::{eval, object::Object},
+        lexer::Lexer,
+        parser::{ast::Node, Parser},
+    };
 
-    let test_cases = vec![
-        TestCase {
-            input: "5",
-            expected: 5,
-        },
-        TestCase {
-            input: "10",
-            expected: 10,
-        },
-    ];
-
-    for test_case in test_cases {
-        let obj = test_eval(test_case.input);
-        test_integer_object(obj, test_case.expected);
-    }
-}
-
-fn test_eval(input: &str) -> Object {
-    let lexer = Lexer::new(input);
-    let mut parser = Parser::new(lexer);
-
-    match parser.parse_program() {
-        Ok(program) => {
-            return eval(Node::Program(program));
+    #[test]
+    fn test_integer_expression() {
+        struct TestCase<'a> {
+            input: &'a str,
+            expected: isize,
         }
-        Err(errors) => {
-            let error_str = errors.join("\n");
-            assert!(
-                false,
-                "The parser encountered {} errors:\n{}",
-                errors.len(),
-                error_str
-            );
-            panic!();
+
+        let test_cases = vec![
+            TestCase {
+                input: "5",
+                expected: 5,
+            },
+            TestCase {
+                input: "10",
+                expected: 10,
+            },
+        ];
+
+        for test_case in test_cases {
+            let obj = test_eval(test_case.input);
+            test_integer_object(obj, test_case.expected);
         }
     }
-}
 
-fn test_integer_object(object: Object, expected: isize) {
-    if let Object::Integer(value) = object {
-        assert_eq!(
-            value, expected,
-            "Expected integer object to contain value {} but got {}",
-            expected, value
-        )
-    } else {
-        assert!(false, "Expected Integer object, got {:?}", object);
+    fn test_eval(input: &str) -> Object {
+        let lexer = Lexer::new(input);
+        let mut parser = Parser::new(lexer);
+
+        match parser.parse_program() {
+            Ok(program) => {
+                return eval(Node::Program(program));
+            }
+            Err(errors) => {
+                let error_str = errors.join("\n");
+                assert!(
+                    false,
+                    "The parser encountered {} errors:\n{}",
+                    errors.len(),
+                    error_str
+                );
+                panic!();
+            }
+        }
+    }
+
+    fn test_integer_object(object: Object, expected: isize) {
+        if let Object::Integer(value) = object {
+            assert_eq!(
+                value, expected,
+                "Expected integer object to contain value {} but got {}",
+                expected, value
+            )
+        } else {
+            assert!(false, "Expected Integer object, got {:?}", object);
+        }
     }
 }
